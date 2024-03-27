0073d754b53f55c22fb569fe20e8d0d498223d51
ahmed <awessam127@gmail.com>
2024-03-28 00:33:38 +0200

    unification done

--- OurResolution.py
+++ OurResolution.py
@@ -2,6 +2,7 @@ import random
 import string
 import copy
 
+
 class Node:
     '''
     Types of Nodes:
@@ -37,7 +38,6 @@ class Node:
                 branch.parents.append(self)
             else:
                 branch.parent = self
-        
 
 
 class Atom:
@@ -93,7 +93,7 @@ def move_negations_inside(root, parent=None):
                 elif branch.name == "¬":
                     branch.name = branch.children[0].children[0].name
                     branch.children = branch.children[0].children[0].children
-                    branch.type = 'Pred'     
+                    branch.type = 'Pred'
                     branch.setParent()
 
             # Quantifier
@@ -136,6 +136,7 @@ def standardize_variables(root, new_atom=None, old_atom=None):
 
 # Move all quantifiers to the left // Quant
 
+
 def collect_quantifiers(root, list_of_quantifiers):
     if type(root) == Atom:
         return
@@ -144,28 +145,30 @@ def collect_quantifiers(root, list_of_quantifiers):
     for branch in root.children:
         collect_quantifiers(branch, list_of_quantifiers)
 
+
 def move_quantifiers_left(root):
-    list_of_quantifiers = [] 
+    list_of_quantifiers = []
     collect_quantifiers(root, list_of_quantifiers)
-    
+
     for q in list_of_quantifiers:
         cur_par = q.parent
         cur_children = q.children
         # skips the quantifier if it is already on the top
-        if(cur_par == None):
+        if (cur_par == None):
             continue
         while cur_par.type == 'Quant':
             cur_par = cur_par.parent
         cur_par.children.remove(q)
         cur_par.children = cur_par.children + cur_children
-    
+
     list_of_quantifiers.reverse()
     new_root = root
     for q in list_of_quantifiers:
         new_root = Node(q.name, q.var, [new_root])
-        
+
     return new_root
-    
+
+
 def set_all_parents(root):
     if root.type == "Quant":
         root.var.parents.append(root)
@@ -188,6 +191,7 @@ def collect_atoms(root, list_of_atoms):
     for branch in root.children:
         collect_atoms(branch, list_of_atoms)
 
+
 def replace_atom_with(root, atom, constant):
     if type(root) == Atom:
         if root.name == atom.name:
@@ -201,18 +205,20 @@ def replace_atom_with(root, atom, constant):
 CONSTANT_NO_NAME = 0
 CONSTANT_PREDICATE = 0
 # Skolemization // Quant
+
+
 def skolemization(root):
-    global CONSTANT_NO_NAME , CONSTANT_PREDICATE
+    global CONSTANT_NO_NAME, CONSTANT_PREDICATE
     set_all_parents(root)
-    
+
     list_of_atoms = []
     list_of_quantifiers = []
-    
+
     collect_atoms(root, list_of_atoms)
     collect_quantifiers(root, list_of_quantifiers)
 
     list_of_atoms.sort(key=lambda atom: atom.name)
-    
+
     universal_flag = False
     list_of_variables = set()
     # Replacing the EXIST's variables with either constants or skolem functions
@@ -220,22 +226,25 @@ def skolemization(root):
         if list_of_quantifiers[i].name == "∀":
             list_of_variables.add(list_of_quantifiers[i].var.name)
             universal_flag = True
-        
+
         if universal_flag:
             if list_of_quantifiers[i].name == "∃":
                 new_atoms = []
                 for atom_name in list_of_variables:
                     new_atoms.append(Atom(atom_name, False))
-                    
-                new_predicate = Node('F'+ str(CONSTANT_PREDICATE), children=new_atoms)
+
+                new_predicate = Node(
+                    'F' + str(CONSTANT_PREDICATE), children=new_atoms)
                 CONSTANT_PREDICATE += 1
-                root = replace_atom_with(root, list_of_quantifiers[i].var, new_predicate)
+                root = replace_atom_with(
+                    root, list_of_quantifiers[i].var, new_predicate)
 
         else:
             if list_of_quantifiers[i].name == "∃":
                 new_constant_atom = Atom("CONST" + str(CONSTANT_NO_NAME), True)
                 CONSTANT_NO_NAME += 1
-                root = replace_atom_with(root, list_of_quantifiers[i].var, new_constant_atom)
+                root = replace_atom_with(
+                    root, list_of_quantifiers[i].var, new_constant_atom)
 
     # Deleting all EXIST Quants
     for q in list_of_quantifiers:
@@ -245,6 +254,8 @@ def skolemization(root):
             par.children.remove(q)
 
 # Drop universal quantifiers // Quant
+
+
 def drop_universal_quantifiers(root):
     if type(root) == Atom:
         return root
@@ -254,9 +265,6 @@ def drop_universal_quantifiers(root):
         root.children[i] = drop_universal_quantifiers(root.children[i])
     return root
 
-
-
-        
 
 # Convert to CNF using the distributive laws // Op
 
@@ -278,30 +286,29 @@ def distribute(root):
             distribute(child)
 
 
-
-
 def generate_random_string(n):
     return ''.join(random.choices(string.ascii_lowercase, k=n))
 
 
 def print_children(root):
-    if(root == None):
+    if (root == None):
         return
     if type(root) == Atom:
         print(root.name, root.is_constant)
         return
     elif root.type == "Quant":
-        print(root.name,'(', root.var.name,')')
+        print(root.name, '(', root.var.name, ')')
     elif root.type == "Pred":
         print(root.name, end=" ( ")
         for branch in root.children:
-            print(branch.name , end=",")
+            print(branch.name, end=",")
         print(' )')
     else:
         print(root.name)
     for branch in root.children:
         if type(branch) == Node:
             print_children(branch)
+
 
 def convert_to_cnf(KB):
     cnf = []
@@ -325,26 +332,35 @@ def tree_to_clauses(sentence):
             clauses.append(child)
     else:
         clauses.append(sentence)
-    
+
     return clauses
 
-def resolve(clause1, clause2):
-    resolved_clauses = []
 
-    for literal1 in clause1:
-        for literal2 in clause2:
+def unification(p1, p2):
+    if p1.name != p2.name:
+        return False
+    if len(p1.children) != len(p2.children):
+        return False
 
-            if isinstance(literal1.children[0], Atom) and isinstance(literal2.children[0], Atom):
-                if literal1.name == literal2.name and literal1.is_constant != literal2.is_constant:
-                    new_clause = [copy.deepcopy(lit) for lit in clause1 + clause2 if lit != literal1 and lit != literal2]
-                    resolved_clauses.append(new_clause)
+    for i in range(len(p1.children)):
+        if p1.children[i].name == p2.children[i].name:
+            continue
+        if (p1.children[i].is_constant) == (p2.children[i].is_constant) and not p1.children[i].is_constant:
+            continue
+        elif (p1.children[i].is_constant) != (p2.children[i].is_constant) and p1.children[i].is_constant:
+            continue
+        elif (p1.children[i].is_constant) != (p2.children[i].is_constant) and p2.children[i].is_constant:
+            continue
+        else:
+            return False
 
-    return resolved_clauses
-
-def resolution(knowledge_base):
-    new_clauses = []
-    for i, clause1 in enumerate(knowledge_base):
-        for j, clause2 in enumerate(knowledge_base):
-            if i != j:
-                new_clauses.extend(resolve(clause1, clause2))
-    return new_clauses
+    for i in range(len(p1.children)):
+        if p1.children[i].name == p2.children[i].name:
+            continue
+        if (p1.children[i].is_constant) == (p2.children[i].is_constant) and not p1.children[i].is_constant:
+            p2.children[i].name = p1.children[i].name
+        elif (p1.children[i].is_constant) != (p2.children[i].is_constant) and p1.children[i].is_constant:
+            p2.children[i].name = p1.children[i].name
+        elif (p1.children[i].is_constant) != (p2.children[i].is_constant) and p2.children[i].is_constant:
+            p1.children[i].name = p2.children[i].name
+    return True
